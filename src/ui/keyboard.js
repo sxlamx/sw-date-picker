eb58f415b881a408fb62c591db585187a7640724
sxlamx <95411960+sxlamx@users.noreply.github.com>
2026-06-12 19:51:20 +0800

    feat(keyboard): keyToAction dispatcher with PageUp/Down + Shift variants

--- src/ui/keyboard.js
+++ src/ui/keyboard.js
@@ -1,2 +1,26 @@
-// TODO(keyboard): arrow/home/end/pageup/pagedown handler.
-export {};
+export const ACTION = Object.freeze({
+  MOVE_DAY: 'move-day',
+  SHIFT_MONTH: 'shift-month',
+  SHIFT_YEAR: 'shift-year',
+  JUMP_START_OF_MONTH: 'jump-start-month',
+  JUMP_END_OF_MONTH: 'jump-end-month',
+  SELECT: 'select',
+});
+
+export function keyToAction(key, shift, _selected) {
+  switch (key) {
+    case 'ArrowLeft': return { type: ACTION.MOVE_DAY, delta: -1 };
+    case 'ArrowRight': return { type: ACTION.MOVE_DAY, delta: 1 };
+    case 'ArrowUp': return { type: ACTION.MOVE_DAY, delta: -7 };
+    case 'ArrowDown': return { type: ACTION.MOVE_DAY, delta: 7 };
+    case 'Home': return { type: ACTION.JUMP_START_OF_MONTH };
+    case 'End': return { type: ACTION.JUMP_END_OF_MONTH };
+    case 'PageUp': return shift ? { type: ACTION.SHIFT_YEAR, delta: -1 } : { type: ACTION.SHIFT_MONTH, delta: -1 };
+    case 'PageDown': return shift ? { type: ACTION.SHIFT_YEAR, delta: 1 } : { type: ACTION.SHIFT_MONTH, delta: 1 };
+    case 'Enter':
+    case ' ':
+      return { type: ACTION.SELECT };
+    default:
+      return null;
+  }
+}
